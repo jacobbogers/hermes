93e910bfb8015ce6cc6efd1bb52d22971cbb11b7
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-08 10:08:00 -0400

    Fixes issue with using `Function` as a type.

--- src/lib/utils/validationFactory.ts
+++ src/lib/utils/validationFactory.ts
@@ -3,7 +3,7 @@ import * as util from 'util';
 export type validation = (s: any) => boolean;
 
 export function validationFactory(v: validation) {
-    return (exec: Function, obj: any, ...rest: any[]) => {
+    return (exec: (...args: any[]) => void, obj: any, ...rest: any[]) => {
         if (v(obj)) {
             const fun = util.format;
             const m = util.format.apply(fun, rest);
