139aaed9f3324ff72814e21b720eb6f836ba3708
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-09 18:14:43 -0400

    Changed concat to push in order to avoid performance issues down the road.

--- src/lib/utils/flatten.ts
+++ src/lib/utils/flatten.ts
@@ -12,13 +12,18 @@ export function flatten(arr: any[]): any[] {
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
         // http://web.mit.edu/jwalden/www/isArray.html
         // tslint:enable
+        // tslint:disable-next-line:typedef
         const { isArray } = Array;
 
         // Recursively tests elements in arr to see if they're arrays.
         // On first run, a is []
-        // If b is an array, flatten b and concatenate it to a.
-        // Otherwise, concatenate the non-array b to a.
-        return arr.reduce((a, b) => a.concat(isArray(b) ? flatten(b) : b), []);
+        // If b is an array, flatten b and push its elements to a.
+        // Otherwise, push the non-array b to a.
+        return arr.reduce(
+            (a: any, b: any): any[] =>
+                (isArray(b) ? a.push(...flatten(b)) : a.push(b)) && a,
+            []
+        );
     } catch {
         // If not provided arguments, or if not given an array, throw an error.
         // This needs to be caught and handled in production.
