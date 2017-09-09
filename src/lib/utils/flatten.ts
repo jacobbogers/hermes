546fdfb55ed00d653d7cc33b430d6d0009dc23e4
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-09 07:04:28 -0400

    Rewrites flatten for clarity and adds tests.

--- src/lib/utils/flatten.ts
+++ src/lib/utils/flatten.ts
@@ -1,12 +1,27 @@
-export function flatten(...rest: any[]): any[] {
-    const rc = [];
-    for (const itm of rest) {
-        if (itm instanceof Array) {
-            const rc2 = flatten(...itm);
-            rc.push(...rc2);
-            continue;
-        }
-        rc.push(itm);
+/**
+ * Flattens an array.
+ *
+ * @export
+ * @param {*[]} arr The array that is to be flattened.
+ * @returns {*[]} The flattened array.
+ */
+export function flatten(arr: any[]): any[] {
+    try {
+        // tslint:disable
+        // Use Array.isArray to test if a value is an array
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+        // http://web.mit.edu/jwalden/www/isArray.html
+        // tslint:enable
+        const { isArray } = Array;
+
+        // Recursively tests elements in arr to see if they're arrays.
+        // On first run, a is []
+        // If b is an array, flatten b and concatenate it to a.
+        // Otherwise, concatenate the non-array b to a.
+        return arr.reduce((a, b) => a.concat(isArray(b) ? flatten(b) : b), []);
+    } catch (e) {
+        // If not provided arguments, or if not given an array, throw an error.
+        // This needs to be caught and handled in production.
+        throw new TypeError('Invalid Type');
     }
-    return rc;
 }
--- test/lib/utils/flatten.test.ts
+++ test/lib/utils/flatten.test.ts
@@ -4,6 +4,29 @@ import { flatten } from '~lib/utils/flatten';
 
 describe('lib/utils/flatten', () => {
     it('Should flatten an object, given it is an array', () => {
-        expect(flatten([1, [2, [3]]])).to.deep.equal([1, 2, 3]);
+        const input = [
+            [1, [2, [3]]],
+            ['a', 'b', ['c', ['d', ['e', ['f', [[['g']]]]]]]],
+            [{ a: 'This', b: 'is' }, [{ c: 'an', d: 'object' }]]
+        ];
+        const output = [
+            [1, 2, 3],
+            ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+            [{ a: 'This', b: 'is' }, { c: 'an', d: 'object' }]
+        ];
+
+        for (let i = 0; i < input.length; i++) {
+            expect(flatten(input[i])).to.deep.equal(output[i]);
+        }
+    });
+
+    it('Should throw a TypeError when provided invalid arguments', () => {
+        expect(flatten).to.throw(TypeError, 'Invalid Type');
+
+        const testTypes = ['string', 1, undefined, null];
+
+        for (const item of testTypes) {
+            expect(() => flatten(item)).to.throw(TypeError, 'Invalid Type');
+        }
     });
 });
