f723a89401f546d5ad254171168bcd43567cbe4f
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-13 16:39:02 -0400

    Clean up tests. Trying to make the test more DRY.

--- test/lib/utils/isNumber.test.ts
+++ test/lib/utils/isNumber.test.ts
@@ -4,22 +4,35 @@ import { isNumber } from '~lib/utils/isNumber';
 
 describe('~lib/utils/isNumber', () => {
     it('Should return true if the given parameter is a finite number.', () => {
-        expect(isNumber(0)).to.be.true;
-        expect(isNumber(2e64)).to.be.true;
-        expect(isNumber(0x11)).to.be.true;
-        expect(isNumber(0b11)).to.be.true;
-        expect(isNumber(0o11)).to.be.true;
-        expect(isNumber(Number.MAX_VALUE)).to.be.true;
-        expect(isNumber(Number.MIN_VALUE)).to.be.true;
+        const testCases = [
+            0,
+            2e64,
+            0x11,
+            0b11,
+            0o11,
+            Number.MAX_VALUE,
+            Number.MIN_VALUE
+        ];
+
+        for (const testCase of testCases) {
+            expect(isNumber(testCase)).to.be.true;
+        }
     });
+
     it('Should return false if the given parameter is not a finite number.', () => {
-        expect(isNumber(null)).to.be.false;
-        expect(isNumber(undefined)).to.be.false;
-        expect(isNumber(+Infinity)).to.be.false;
-        expect(isNumber(-Infinity)).to.be.false;
-        expect(isNumber('Hello World!')).to.be.false;
-        expect(isNumber('0')).to.be.false;
-        expect(isNumber({ an: 'Object' })).to.be.false;
-        expect(isNumber([ 'an', 'Object' ])).to.be.false;
+        const testCases = [
+            null,
+            undefined,
+            +Infinity,
+            -Infinity,
+            'Hello World!',
+            '0',
+            { an: 'Object' },
+            ['an', 'Object']
+        ];
+
+        for (const testCase of testCases) {
+            expect(isNumber(testCase)).to.be.false;
+        }
     });
 });
