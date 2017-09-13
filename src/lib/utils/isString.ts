123d40bf2dd9849ecfecf46111c5f9d41613c336
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-13 16:39:19 -0400

    Documents and tests ~lib/utils/isString.

--- src/lib/utils/isString.ts
+++ src/lib/utils/isString.ts
@@ -1,3 +1,10 @@
+/**
+ * Determines whether the given parameter is a string.
+ *
+ * @export
+ * @param {*} s An object of any type.
+ * @returns {boolean} Return true if the given parameter is a string, otherwise false.
+ */
 export function isString(s: any): s is string {
     return typeof s === 'string';
 }
