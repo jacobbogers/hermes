4f8f81ef9c95868382b3bf26623579d103b58643
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-16 08:10:43 -0400

    Tests ~lib/utils/OperationResult
    No documentation, yet. Also this._collected initializer is weird, needs some discussion.

--- src/lib/utils/OperationResult.ts
+++ src/lib/utils/OperationResult.ts
@@ -1,20 +1,26 @@
 export class OperationResult<I> {
+
     private _inserted: number | undefined;
     private _errors = 0;
     private _collected: I[] | undefined;
     private _deleted: number | undefined;
+
     public get inserted() {
         return this._inserted;
     }
+
     public get errors() {
         return this._errors;
     }
+
     public get collected(): I[] | undefined {
         return this._collected;
     }
+
     public get first(): I | undefined {
         return this._collected && this._collected[0];
     }
+
     public constructor(props: {
         inserted?: number;
         errors: number;
@@ -24,6 +30,7 @@ export class OperationResult<I> {
         this._inserted = props.inserted;
         this._errors = props.errors;
         this._deleted = props.deleted;
-        this._collected = props.collected && props.collected.splice(0);
+        // this._collected = props.collected && props.collected.splice(0); ???
+        this._collected = props.collected;
     }
 }
