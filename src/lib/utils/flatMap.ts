fecf1198f10a69b3bb22056793fc084e25d42376
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2017-09-16 08:15:20 -0400

    Documents and tests ~lib/utils/flatMap.

--- src/lib/utils/flatMap.ts
+++ src/lib/utils/flatMap.ts
@@ -1,18 +1,27 @@
-// tslint:disable:typedef
+/**
+ * Flattens a Map of n-dimensions.
+ *
+ * @export
+ * @template T An object of any type
+ * @template F not sure???
+ * @template Mc A map whose values are either nested maps, or some other type.
+ * @param {Map} map An n-dimensional map.
+ * @returns {*[]} A 1D array of values of any type.
+ */
 export function flatMap<
     T,
     F extends { obj: T },
     Mc extends Map<T[keyof T], Mc | F>
 >(map: Mc): F[] {
-    const rc: F[] = [];
+    const flattened: F[] = [];
     for (const itm of map.values()) {
         if (itm instanceof Map) {
-            const rc2 = flatMap(itm);
-            rc.push(...(rc2 as F[]));
+            const rc2: F[] = flatMap(itm);
+            flattened.push(...rc2);
             continue;
         }
-        rc.push(itm);
+        flattened.push(itm);
     }
 
-    return rc;
+    return flattened;
 }
