f0a16abd043b33b86a72d8e042feac12295d7166
jacobbogers <jkfbogers@gmail.com>
2018-02-22 01:24:18 +0100

    1. We now use "webpack-helpers" module instead of rolling our own loaders and plugins, hence the removal of explicit dependences of webpack and its loaders/plugins

--- config/webpack/_module.js
+++
@@ -1,5 +0,0 @@
-const { flatten } = require('./tools');
-const { inliner, raw, styles, ts, tslint, fonts } = require('./loaders');
-module.exports = {
-    rules: flatten([inliner, raw, styles, tslint, ts, fonts])
-};
--- config/webpack/_resolve.js
+++
@@ -1,7 +0,0 @@
-const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
-
-module.exports = {
-    // try to auto resolve files names without explicit extentions,if they have these extentions
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.scss', '.sass', '.css', '.svg', '.woff', '.ttf', '.eot'],
-    plugins: [new TsConfigPathsPlugin()]
-};
